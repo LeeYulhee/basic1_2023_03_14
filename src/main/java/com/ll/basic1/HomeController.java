451c6fcfc819fd9ad6ea7afe4a01dbcb7c270919
LeeYulhee <reasonoflife39@gmail.com>
2023-03-14 11:44:32 +0900

    home/increase 혼자 풀기

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -11,4 +11,25 @@ public class HomeController {
     public String showMain(){
         return "안녕하세요.";
     }
+
+    @GetMapping("/home/main2")
+    @ResponseBody
+    public String showMain2(){
+        return "반갑습니다.";
+    }
+
+    @GetMapping("/home/main3")
+    @ResponseBody
+    public String showMain3(){
+        return "즐거웠습니다.";
+    }
+
+    int i = -1;
+
+    @GetMapping("/home/increase")
+    @ResponseBody
+    public String showIncrease () {
+        i++;
+        return "응답 : " + i;
+    }
 }
